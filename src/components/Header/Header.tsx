74b5c3c4cecbad92478ef0a039b04d610a504a33
Monkey <duyk16@gmail.com>
2023-06-07 14:35:25 +0700

    update connect wallet button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { shortenAddress } from '@/utils'
 import Link from 'next/link'
 
 export const Header = () => {
-  const { address } = useWeb3()
+  const { address, connectWallet } = useWeb3()
 
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
@@ -24,7 +24,7 @@ export const Header = () => {
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              onClick={() => {}}
+              onClick={connectWallet}
             >
               Connect wallet
             </button>
